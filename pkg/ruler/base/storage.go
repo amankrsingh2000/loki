f3d299dbbedfe2af5425470a9dcb32411d9c1617
Aditya C S <aditya.gnu@gmail.com>
2023-03-17 10:32:36 +0530

    check co-authors are added (#10)
    * cos client creation code and unit tests
    
    * Added GetObject,PutObject and IsObjectNotFoundErr code and unit tests
    
    Co-authored-by: Suruthi-G-K <shruthi.suruthi@gmail.com>
    
    * add ListObject and DeleteObject methods and unit tests
    
    Rebasing the code
    
    Update config and test files with new changes
    
    Update unit test for List
    
    Arrange methods belong to mockCosClient together
    
    Updated test cases to run independently
    
    Co-authored-by: tareqmamari <tariq.mamari@de.ibm.com>
    
    * add support for IAM API Key Authentication
    
    Added code to support IAM API Key Authentication
    
    Update the conditions for api key
    
    use default auth endpoint for cos
    
    Use default auth endpoint instead of forcing to pass auth endpoint always
    for COS storage provider.
    
    Replace const to defaultCOSAuthEndpoint
    
    Replace DEFAULT_COS_AUTH_ENDPOINT with defaultCOSAuthEndpoint
    
    Also fixed error fmt.Errorf call needs 6 args but has 7 args
    
    Added new cases in Test_COSConfig
    
    Added unit test for IAM API Key Authentication
    
    iam api key auth test formatting
    
    These are just minor formatting changes for IAM API key authentication
    tests.
    
    lowercase clifags cos prefix and rename to hedgedCOS
    
    Co-authored-by: tareqmamari <tariq.mamari@de.ibm.com>
    Co-authored-by: amankrsingh2000 <amankrsingh2110@gmail.com>
    
    * add documentation for COS storage (#8)
    
    * Add documentation for COS as storage
    
    * Address review comments
    
    ---------
    
    Co-authored-by: Aman Kumar Singh <amankrsingh2110@gmail.com>
    Co-authored-by: Suruthi-G-K <shruthi.suruthi@gmail.com>
    Co-authored-by: “athira <athira.varghese@ibm.com>
    Co-authored-by: tareqmamari <tariq.mamari@de.ibm.com>
    Co-authored-by: “athira <athirava1994@gmail.com>
    Co-authored-by: Shahul <40440454+shahulsonhal@users.noreply.github.com>

--- pkg/ruler/base/storage.go
+++ pkg/ruler/base/storage.go
@@ -25,6 +25,7 @@ import (
 	"github.com/grafana/loki/pkg/storage/chunk/client/baidubce"
 	"github.com/grafana/loki/pkg/storage/chunk/client/gcp"
 	"github.com/grafana/loki/pkg/storage/chunk/client/hedging"
+	"github.com/grafana/loki/pkg/storage/chunk/client/ibmcloud"
 	"github.com/grafana/loki/pkg/storage/chunk/client/openstack"
 )
 
@@ -40,6 +41,7 @@ type RuleStoreConfig struct {
 	S3           aws.S3Config              `yaml:"s3" doc:"description=Configures backend rule storage for S3."`
 	BOS          baidubce.BOSStorageConfig `yaml:"bos" doc:"description=Configures backend rule storage for Baidu Object Storage (BOS)."`
 	Swift        openstack.SwiftConfig     `yaml:"swift" doc:"description=Configures backend rule storage for Swift."`
+	COS          ibmcloud.COSConfig        `yaml:"cos" doc:"description=Configures backend rule storage for IBM Cloud Object Storage (COS)."`
 	Local        local.Config              `yaml:"local" doc:"description=Configures backend rule storage for a local file system directory."`
 
 	mock rulestore.RuleStore `yaml:"-"`
@@ -54,6 +56,7 @@ func (cfg *RuleStoreConfig) RegisterFlags(f *flag.FlagSet) {
 	cfg.Swift.RegisterFlagsWithPrefix("ruler.storage.", f)
 	cfg.Local.RegisterFlagsWithPrefix("ruler.storage.", f)
 	cfg.BOS.RegisterFlagsWithPrefix("ruler.storage.", f)
+	cfg.COS.RegisterFlagsWithPrefix("ruler.storage.", f)
 	f.StringVar(&cfg.Type, "ruler.storage.type", "", "Method to use for backend rule storage (configdb, azure, gcs, s3, swift, local, bos)")
 }
 
@@ -68,6 +71,11 @@ func (cfg *RuleStoreConfig) Validate() error {
 	if err := cfg.S3.Validate(); err != nil {
 		return errors.Wrap(err, "invalid S3 Storage config")
 	}
+	/* currently cos does not support storage class so we don't add validation
+	if err := cfg.COS.Validate(); err != nil {
+		return errors.Wrap(err, "invalid COS Storage config")
+	}
+	*/
 	return nil
 }
 
@@ -102,6 +110,8 @@ func NewLegacyRuleStore(cfg RuleStoreConfig, hedgeCfg hedging.Config, clientMetr
 		client, err = baidubce.NewBOSObjectStorage(&cfg.BOS)
 	case "swift":
 		client, err = openstack.NewSwiftObjectClient(cfg.Swift, hedgeCfg)
+	case "cos":
+		client, err = ibmcloud.NewCOSObjectClient(cfg.COS, hedgeCfg)
 	case "local":
 		return local.NewLocalRulesClient(cfg.Local, loader)
 	default:
